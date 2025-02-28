24cdfc341e795baf228edac252e7b418d0c89cc8
youseokhwan <youseokhwan15@gmail.com>
2025-02-28 15:53:47 +0900

    feat: useFootnote 기능 구현

--- src/hooks/useFootnote.ts
+++ src/hooks/useFootnote.ts
@@ -2,11 +2,29 @@ import { useEffect } from "react"
 
 const useFootnote = () => {
   useEffect(() => {
-    const footnotes = document.querySelectorAll("sup a")
+    const refs = document.querySelectorAll("sup .footnote-ref")
+    const backRefs = document.querySelectorAll(".footnotes .footnote-backref")
+  
+    const handleClick = (e: Event) => {
+      e.preventDefault()
+      
+      const targetId = (e.target as HTMLAnchorElement).getAttribute("href")?.slice(1)
 
-    footnotes.forEach((link) => {
-      link.classList.add("useFootnoteTest")
-    })
+      if (targetId) {
+        const targetElement = document.getElementById(targetId)
+        if (targetElement) {
+          targetElement.scrollIntoView({ behavior: "smooth" })
+        }
+      }
+    }
+  
+    refs.forEach((link) => link.addEventListener("click", handleClick))
+    backRefs.forEach((link) => link.addEventListener("click", handleClick))
+  
+    return () => {
+      refs.forEach((link) => link.removeEventListener("click", handleClick))
+      backRefs.forEach((link) => link.removeEventListener("click", handleClick))
+    }
   }, [])
 }
 
