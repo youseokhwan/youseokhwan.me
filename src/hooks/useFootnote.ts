881d4b8a24f9d74a1aff5ce6f9c6841fb6fe5c97
youseokhwan <youseokhwan15@gmail.com>
2025-02-28 17:26:18 +0900

    refactor: 개행 방식 통일

--- src/hooks/useFootnote.ts
+++ src/hooks/useFootnote.ts
@@ -22,6 +22,7 @@ const useFootnote = () => {
 
       if (targetId) {
         const targetElement = document.getElementById(targetId)
+
         if (targetElement) {
           targetElement.scrollIntoView({ behavior: "smooth" })
         }
