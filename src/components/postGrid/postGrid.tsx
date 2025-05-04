816b22df26314c76790354380610d01bf48775a0
youseokhwan <youseokhwan15@gmail.com>
2025-05-04 17:32:42 +0900

    refactor: 사용하지 않는 timeToRead 변수 제거

--- src/components/postGrid/postGrid.tsx
+++ src/components/postGrid/postGrid.tsx
@@ -25,7 +25,7 @@ const PostGrid: React.FC<PostGridProperties> = ({ posts }) => {
   return (
     <Grid role="list">
       {currentList.map(data => {
-        const { id, slug, title, desc, date, category, thumbnail, timeToRead } = data
+        const { id, slug, title, desc, date, category, thumbnail } = data
         const ariaLabel = `${title} - ${category} - Posted on ${date}`
         return (
           <List key={id} role="listitem">
@@ -36,7 +36,6 @@ const PostGrid: React.FC<PostGridProperties> = ({ posts }) => {
                 title={title}
                 desc={desc}
                 date={date}
-                timeToRead={timeToRead}
               />
             </Link>
           </List>
